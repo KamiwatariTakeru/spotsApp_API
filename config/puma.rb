5bbf9466a1d4fca9ba092842e027532e7f0dee08
KamiwatariTakeru <takeru.shonan@gmail.com>
2024-06-16 02:10:24 +0900

    Update Puma configuration to listen on 0.0.0.0

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,21 @@
+# Puma設定ファイル
+
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+
+# 既存のport設定行を置き換える
+port        ENV.fetch("PORT") { 3000 }, "0.0.0.0"
+
+environment ENV.fetch("RACK_ENV") { 'development' }
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  # Rails 4.1+ のワーカー固有の設定
+  # 参照: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
