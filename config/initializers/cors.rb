c54b9872fc450ad5e16a02aa38750c74e5fe864e
KamiwatariTakeru <takeru.shonan@gmail.com>
2024-07-31 03:13:23 +0900

    change origins of cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://spots-app-frontend.vercel.app'
+    origins 'https://spots-post-front.vercel.app'
 
     resource '*',
       headers: :any,
