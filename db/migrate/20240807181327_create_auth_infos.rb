517c18a2d53e29daf2ce9271a6001b3deb63eede
KamiwatariTakeru <takeru.shonan@gmail.com>
2024-08-08 04:47:25 +0900

    evaluations,likesマイグレーションファイル削除

--- db/migrate/20240807181327_create_auth_infos.rb
+++ db/migrate/20240807181327_create_auth_infos.rb
@@ -1,12 +1,8 @@
 class CreateAuthInfos < ActiveRecord::Migration[6.0]
   def change
-    create_table :auth_infos, id: :string do |t|
+    create_table :auth_infos do |t|
       t.string :provider
-      t.bigint :user_id, null: false
-      t.timestamps
+      t.references :user, null: false, foreign_key: true
     end
-
-    add_index :auth_infos, :user_id
-    add_foreign_key :auth_infos, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,55 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_07_190653) do
+ActiveRecord::Schema.define(version: 2024_08_07_194451) do
 
-  create_table "auth_infos", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "provider"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_auth_infos_on_user_id"
-  end
-
-  create_table "evaluations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "spot_id", null: false
-    t.integer "starsAmount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["spot_id"], name: "index_evaluations_on_spot_id"
-    t.index ["user_id"], name: "index_evaluations_on_user_id"
-  end
-
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "spot_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["spot_id"], name: "index_likes_on_spot_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "stars_sum"
-    t.float "stars_avg"
-    t.float "latitude", limit: 53
-    t.float "longitude", limit: 53
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "email"
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "evaluations", "spots"
-  add_foreign_key "evaluations", "users"
-  add_foreign_key "likes", "spots"
-  add_foreign_key "likes", "users"
 end
